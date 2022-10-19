01a3b6f0a90a87963eca96717d35617f07f9590c
Tearai <77899947+Tearai@users.noreply.github.com>
2022-10-19 13:32:46 +0100

    Revert "Lab 1"
    This reverts commit ac2aaa2d3c092b619d9494330466f9f0bfae2e7c.

--- GE1 Examples 2022/Assets/Cube1.cs
+++ GE1 Examples 2022/Assets/Cube1.cs
@@ -13,6 +13,6 @@ public class Cube1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0, 1 * Time.deltaTime);
+        transform.Translate(0, 0, 1);
     }
 }
